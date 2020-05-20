5c7ad6b4d583a4558be6b90d43310b753433d57c
bomwo <bomwo25@gmail.com>
2020-05-20 21:39:47 +0900

    Fix tracking id

--- gatsby-config.js
+++ gatsby-config.js
@@ -125,7 +125,7 @@ module.exports = {
     'gatsby-plugin-sharp',
     {
       resolve: 'gatsby-plugin-google-analytics',
-      options: { trackingId: 'UA-73379983-2' },
+      options: { trackingId: 'UA-167184578-1' },
     },
     {
       resolve: 'gatsby-plugin-google-fonts',
